30a9331163806712c9ca24216444f6b09174249e
Michael Krause <michael.krause@nps.edu>
2023-10-16 09:16:04 -0700

    Corrected a few lines where figure format was hard-coded

--- src/analise_thesis/plotter.py
+++ src/analise_thesis/plotter.py
@@ -8,7 +8,7 @@ from src.plot_utils import PlotUtils
 
 class Plotter:
     def __init__(self, data: list[tuple], nrows: int, ncols: int, xlabel: str, ylabel: str,
-                 xlim: list, ylim: list, capsize: int, fig_format: str = 'png') -> None:
+                 xlim: list, ylim: list, capsize: int, fig_format: str) -> None:
         """
         @param data:
         @param nrows:
@@ -18,10 +18,8 @@ class Plotter:
         @param xlim:
         @param ylim:
         @param capsize:
-        @type fig_format: (str) default is 'png', but can be set to 'svg' for vector graphics
+        @type fig_format: (str) 'png' or 'svg' for vector graphics
         """
-
-        self.fig_format = fig_format
         self.data: list[tuple] = data
         self.SAMPLE_NUM_CUTOFF = 1
         self.nrows = nrows
@@ -31,6 +29,7 @@ class Plotter:
         self.xlim = xlim
         self.ylim = ylim
         self.capsize = capsize
+        self.fig_format = fig_format
 
     def run_individual_chips(self) -> None:
         """Plot all injections of a single chip ID and write plot to disk"""
@@ -40,7 +39,7 @@ class Plotter:
                 xdata, ydata, yerr = channel_data.get_data()
                 xticks = list(range(1, channel_data.num_injections + 1))
                 title = f'Chip type: {datum[2]}, id: {datum[1]}, width: {datum[0]}'
-                figname = f'chip_id_{datum[1]}_type_{datum[2]}_width_{datum[0]}.png'
+                figname = f'chip_id_{datum[1]}_type_{datum[2]}_width_{datum[0]}.{self.fig_format}'
                 PlotUtils.plot_scatter(xdata, ydata, yerr=yerr, nrows=self.nrows, ncols=self.ncols,
                                        figname=figname, fig_format=self.fig_format,
                                        title=title, capsize=self.capsize, xlabel=self.xlabel, ylabel=self.ylabel,
@@ -77,7 +76,6 @@ class Plotter:
         if not restructured_data:
             return None
         print(f'data type of restructured_data: {str(type(restructured_data))}')
-        # pprint(f'restructured_data: {restructured_data}')
         for item in restructured_data:
             pprint(f'item: {item}')
         tups: list = []
@@ -90,10 +88,9 @@ class Plotter:
         out_data['number_of_injections'] = len(xdata)
         out_data['y_data'] = weighted_ydata
         out_data['y_err'] = weighted_yerr
-        # out_data['weighted_yerr'] = weighted_yerr
 
         title = f'Weighted average: Chip type: {chip_type}, width: {chip_width}'
-        figname = f'width_{chip_width}_type_{chip_type}_weighted_data.png'
+        figname = f'width_{chip_width}_type_{chip_type}_weighted_data.{self.fig_format}'
         print(f'printing: {figname}')
 
         # Ticks on the x-axis are the chip widths we measured from:
