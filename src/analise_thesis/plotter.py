d89b6401d47ebc951ff4792099f6550403b714bb
Michael Krause <michael.krause@nps.edu>
2023-10-24 11:24:26 -0700

    Added tests

--- src/analise_thesis/plotter.py
+++ src/analise_thesis/plotter.py
@@ -16,22 +16,26 @@ class MyList(list):
 
 class Plotter:
     def __init__(self, data: list[tuple], nrows: int, ncols: int, xlabel: str, ylabel: str, xlim: list, ylim: list,
-                 aspect: int, capsize: int, fig_format: str, fontsize: float = 12.0) -> None:
+                 aspect: int, capsize: int, fig_format: str, is_title_and_labels: bool = True,
+                 fontsize: float = 12.0) -> None:
         """
 
-        @param data:
-        @param nrows:
+        @param data:    a list of one or more three-tuples, which represents data of one channel width on one chip of a
+                        specific type, e.g., (896, 1, 'hard')
+        @param nrows:   refers to the rows and columns of a figure, respectively
         @param ncols:
-        @param xlabel:
+        @param xlabel:  figure x- and y- labels
         @param ylabel:
-        @param xlim:
+        @param xlim:    figure x- and y-limits
         @param ylim:
-        @type aspect: (float) set the aspect ratio of x- and y-axis, regardless of data ranges
-        @param fontsize: font size for ticks on x- and y-axes
-        @param capsize:
+        @type aspect:   (float) set the aspect ratio of x- and y-axis, regardless of data ranges
+        @param capsize: the length of the horizontal line on top and bottom of an error bar
         @type fig_format: (str) 'png' or 'svg' for vector graphics
+        @type is_title_and_labels: (bool) figure has title and x- and y-labels if true
+        @param fontsize: font size for ticks on x- and y-axes in points
         """
 
+        self.is_title_and_labels = is_title_and_labels
         self.data: list[tuple] = data
         self.SAMPLE_NUM_CUTOFF = 1
         self.nrows = nrows
@@ -44,6 +48,7 @@ class Plotter:
         self.capsize = capsize
         self.fig_format = fig_format
         self.fontsize = fontsize
+        self.is_title_and_labels = is_title_and_labels
 
     def run_individual_chips(self) -> None:
         lxlim = 0
@@ -57,14 +62,18 @@ class Plotter:
                 xticks = list(range(1, len(xdata) + 1))  # list(range(1, channel_data.num_injections + 1))
                 title = f'Chip type: {datum[2]}, id: {datum[1]}, width: {datum[0]}'
                 figname = f'chip_id_{datum[1]}_type_{datum[2]}_width_{datum[0]}.{self.fig_format}'
-                # PlotUtils.plot_scatter(xdata, ydata, yerr=yerr, nrows=self.nrows, ncols=self.ncols, title=title,
-                #                        xlabel=self.xlabel, ylabel=self.ylabel, xlim=[lxlim, rxlim], xticks=xticks,
-                #                        fontsize=self.fontsize, figname=figname, fig_format=self.fig_format,
-                #                        aspect=self.aspect, capsize=self.capsize, colors=None)
-                PlotUtils.plot_scatter(xdata, ydata, yerr=yerr, nrows=self.nrows, ncols=self.ncols, xlim=[lxlim, rxlim],
-                                       xticks=xticks, fontsize=self.fontsize, figname=figname,
-                                       fig_format=self.fig_format, aspect=self.aspect, capsize=self.capsize,
-                                       colors=None)
+                if self.is_title_and_labels:
+                    PlotUtils.plot_scatter(
+                        xdata, ydata, yerr=yerr, nrows=self.nrows, ncols=self.ncols, title=title,
+                        xlabel=self.xlabel, ylabel=self.ylabel, xlim=[lxlim, rxlim], xticks=xticks,
+                        fontsize=self.fontsize, figname=figname, fig_format=self.fig_format,
+                        aspect=self.aspect, capsize=self.capsize, colors=None)
+                else:
+                    PlotUtils.plot_scatter(
+                        xdata, ydata, yerr=yerr, nrows=self.nrows, ncols=self.ncols, xlim=[lxlim, rxlim],
+                        xticks=xticks, fontsize=self.fontsize, figname=figname,
+                        fig_format=self.fig_format, aspect=self.aspect, capsize=self.capsize,
+                        colors=None)
 
     def get_averaged_channel_data(self, data_subset: list[tuple]) -> dict | None:
         """Return weighted average and standard deviation for one channel width from all chips
@@ -84,16 +93,14 @@ class Plotter:
             return None
         channel_data: list = []
         for id_ in data_subset:
-            print(f'processing chip ID: {id_}')
+            print(f'processing chip ID: {id_[1]}')
             channel_data.append(ChannelData(channel_width=id_[0], chip_id=id_[1], chip_type=id_[2]))
 
         # Aggregate data:
-        xdata, ydata__aggregated = self.aggregate_ydata_yerr_from_one_channel(channel_data)
-        for d in ydata__aggregated:
-            print(f'item in ydata__aggregated: {d}')
+        xdata, ydata_yerr_agg = self.aggregate_from_one_channel(channel_data)
 
         # Restructure data:
-        restructured_data = Restructure.restructure(tuple(ydata__aggregated))
+        restructured_data = Restructure.restructure(tuple(ydata_yerr_agg))
         if not restructured_data:
             return None
         print(f'data type of restructured_data: {str(type(restructured_data))}')
@@ -105,8 +112,14 @@ class Plotter:
             tups.append((weighted_ydata_, weighted_yerr_))
             print(f'weighted_ydata_: {weighted_ydata_}, weighted_yerr_: {weighted_yerr_}')
         weighted_ydata, weighted_yerr = PlotUtils.lst_tuples_to_lists(data=tups)
-        out_data['x_data'] = xdata
-        out_data['number_of_injections'] = len(xdata)
+
+        # assert(len(xdata) == len(list(range(1, len(ydata_yerr_agg) + 1))))  # FAILS!
+
+        max_number_injections = max([ch.num_injections for ch in channel_data])
+
+        out_data['x_data'] = xdata  # list(range(1, max_number_injections))
+        print(f'xdata: {xdata}; max_number_injections: {max_number_injections}')
+        out_data['number_of_injections'] = max_number_injections
         out_data['y_data'] = weighted_ydata
         out_data['y_err'] = weighted_yerr
 
@@ -137,18 +150,20 @@ class Plotter:
         return list(filter(lambda x: x[0] == width, self.data))
 
     @staticmethod
-    def aggregate_ydata_yerr_from_one_channel(channel_data: list) -> tuple:
-        """Return a list of ydata and yerr from one channel width"""
+    def aggregate_from_one_channel(channel_data: list[ChannelData]) -> tuple:
+        """Return a list of tuples. Each tuple contains 3 lists: xdata, ydata and yerr from one channel width"""
+        # print(f'channel_data: {[channel_datum for channel_datum in channel_data]}')
         xdata = []
-        ydata_aggregated = []
+        ydata_yerr_aggregated = []
         for channel_datum in channel_data:
             print(f'aggregate from channel_datum: {channel_datum}')
+            print(channel_datum.df)
             xdata_, ydata, yerr = channel_datum.get_data()
             if len(xdata_) > len(xdata):
                 xdata = xdata_
             print(f'xdata: {xdata}\nydata: {ydata}\nyerr: {yerr}\n')
-            ydata_aggregated.append((ydata, yerr))
-        return xdata, ydata_aggregated
+            ydata_yerr_aggregated.append((ydata, yerr))
+        return xdata, ydata_yerr_aggregated
 
     @staticmethod
     def sort_tuples(data: list, idx_to_sort_by: int = 0) -> list | None:
--- test_/analise_thesis/test_plotter.py
+++ test_/analise_thesis/test_plotter.py
@@ -1,21 +1,12 @@
 #!/usr/bin/env python3
 import unittest
-
+from test_.analise_thesis.set_up import SetUp
+from unittest.mock import patch, PropertyMock
+from src.analise_thesis.channel_data import ChannelData
 from src.analise_thesis.plotter import Plotter
 
 
-class TestPlotter(unittest.TestCase):
-
-    def setUp(self):
-        data_hard = [
-            (100, 1, 'hard'),
-            (200, 1, 'hard'),
-            (100, 2, 'hard'),
-            (100, 3, 'hard'),
-        ]
-
-        self.plotter_test = Plotter(data=data_hard, nrows=1, ncols=1, xlabel="injection number", ylabel='R [K$\Omega$]',
-                                    xlim=[0, 5], ylim=[0, 1e6], fontsize=12, fig_format='png', capsize=10)
+class TestPlotter(SetUp):
 
     def test_filter_by_width(self):
         data_from_single_channel_width = self.plotter_test.filter_by_width(width=100)
@@ -26,6 +17,34 @@ class TestPlotter(unittest.TestCase):
         channels = self.plotter_test.get_channel_list()
         expected = [100, 200]
         self.assertEqual(expected, channels)
+
+    @patch('src.analise_thesis.config.Config.spreadsheet_filename', new_callable=PropertyMock,
+           return_value='test_data.xlsx')
+    @patch('src.analise_thesis.channel_data.ChannelData._ChannelData__get_sheetname')
+    @patch('src.analise_thesis.config.Config.excel_spreadsheet_path', new_callable=PropertyMock)
+    def test_aggregate_ydata_yerr_from_one_channel(self, mock_path, mock_get_sheetname, mock_spreadsheet_name):
+        """Test whether all lists in a tuple (which is from a list of tuples) are of same length"""
+        # Some setup work:
+        mock_path.return_value = self.test_data_path
+        mock_get_sheetname.return_value = 'Foo Bar Data1'
+        channel_data = [ChannelData(chip_type='hard', chip_id=1, channel_width=896),
+                        ChannelData(chip_type='hard', chip_id=1, channel_width=764),
+                        ChannelData(chip_type='hard', chip_id=4, channel_width=192)]
+
+        # Now testing:
+        channel_data_aggregated: list[tuple] = Plotter.aggregate_from_one_channel(
+            channel_data=channel_data)
+        for tup in channel_data_aggregated:
+            print(tup)
+            observed = [
+                (len(tup[0]) == len(tup[1])),
+                (len(tup[0]) == len(tup[2])),
+                (len(tup[1]) == len(tup[2]))
+            ]
+            self.assertTrue(all(observed))
+
+    def test_get_averaged_channel_data(self):
+        pass
 
 
 if __name__ == '__main__':
